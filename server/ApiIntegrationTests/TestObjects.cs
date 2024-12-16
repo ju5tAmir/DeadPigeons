8153e1d4352f58ebaa0b14e36279d86c0b0b930e
Ju5tAmir <amirbakhshizad53@gmail.com>
2024-12-16 16:01:00 +0100

    Unit tests for game endpoints

--- server/ApiIntegrationTests/TestObjects.cs
+++ server/ApiIntegrationTests/TestObjects.cs
@@ -1,3 +1,4 @@
+using System.Runtime.InteropServices.JavaScript;
 using Bogus;
 using DataAccess.Entities;
 
@@ -17,5 +18,25 @@ public class TestObjects
             .RuleFor(p => p.IsActive, true)
             .RuleFor(p => p.IsAutoPlay, false)
             .RuleFor(p => p.RegistrationDate, f => DateTime.UtcNow);
+    }
+    
+    public static Game GetGame()
+    {
+        return new Faker<Game>()
+            .RuleFor(p => p.GameId, f => Guid.NewGuid())
+            .RuleFor(p => p.Year, 2024)
+            .RuleFor(p => p.WeekNumber, 50)
+            .RuleFor(p => p.OnlinePlayers, f => f.Random.Int(1, 100))
+            .RuleFor(p => p.OnlineWinningPlayers, f => f.Random.Int(1, 5))
+            .RuleFor(p => p.OnlineBoards, f => f.Random.Int(1, 300))
+            .RuleFor(p => p.OnlineWinningBoards, f => f.Random.Int(5, 20))
+            .RuleFor(p => p.OnlineIncome, f => f.Random.Int(500, 2000))
+            .RuleFor(p => p.OnlinePayout, f => f.Random.Int(200, 1000))
+            .RuleFor(p => p.OfflinePlayers, f => f.Random.Int(1, 100))
+            .RuleFor(p => p.OfflineWinningPlayers, f => f.Random.Int(1, 5))
+            .RuleFor(p => p.OfflineBoards, f => f.Random.Int(1, 300))
+            .RuleFor(p => p.OfflineWinningBoards, f => f.Random.Int(5, 20))
+            .RuleFor(p => p.OfflineIncome, f => f.Random.Int(500, 2000))
+            .RuleFor(p => p.OfflinePayout, f => f.Random.Int(200, 1000));
     }
 }
